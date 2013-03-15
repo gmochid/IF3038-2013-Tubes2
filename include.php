df4706f60e8950e64f66288e74660cd1d6fcb61c
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-03-15 08:49:38 +0700

    add global database variable and include file

---
+++ include.php
@@ -0,0 +1,5 @@
+<?php
+	include_once 'utilities/db.php';
+	include_once 'model/BaseModel.php';
+    include_once 'model/User.php';
+?>
---
+++ utilities/db.php
@@ -0,0 +1,10 @@
+<?php
+    global $host;
+	global $username;
+	global $password;
+	global $dbname;
+	$host = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname = 'task_manajement';
+?>
