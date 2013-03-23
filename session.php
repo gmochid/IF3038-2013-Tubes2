cb0af8f1e3150107a90dee83645035dc66b5ca12
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-03-23 21:27:26 +0700

    added login function (not tested)

--- session.php
+++ session.php
@@ -1,14 +1,32 @@
 <?php
 	include_once dirname(__FILE__).'\include.php';
 
-    if($_GET['action'] == 'create') {
-    	echo session_start()."<br>";
-		$a = $_GET['username'];
-		$_SESSION['username'] = $a; 
-		echo session_id();
-		//header('Location: tes.php');
+	
+	if($_SERVER['REQUEST_METHOD'] == 'post') {
+		if($_GET['action'] == 'login') {
+			$user = new User($_POST['username']);
+			if($user->fullname == "") {
+				echo "User not found<br>";
+				echo "<a href='index.html'>return</a>";
+			} else if($user->authenticate($_POST['password'])) {
+				session_start();
+				$_SESSION['username'] = $_GET['username'];
+				header('Location: src/dashboard.php');
+			} else {
+				echo "Password not match<br>";
+				echo "<a href='index.html'>return</a>";
+			}
+		} else {
+			session_start();
+		}
+	} else if($_GET['action'] == 'create') {
+		session_start();
+		$_SESSION['username'] = $_GET['username'];
+		header('Location: src/dashboard.php');
     } else {
+    	session_start();
     	session_destroy();
 		unset($_SESSION['username']);
+		header('Location: index.php');
     }
 ?>
